2f1a95ebe558dfc2065cea014dbddc3c6e61ec13
mohamedsaid321 <mohamedsaidumn@gmail.com>
2023-02-27 22:23:34 -0600

    used a button click that changes title using useState to rerender the component

--- src/components/Expenses/ExpenseItem.tsx
+++ src/components/Expenses/ExpenseItem.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React, { useState } from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
@@ -10,13 +10,23 @@ type ExpenseItemParameterType = {
 };
 
 const ExpenseItem = (props: ExpenseItemParameterType) => {
+  // function clickHandler() {}
+  const [title, setTitle] = useState(props.title);
+  console.log("ExpenseItem evaluated by React");
+
+  const clickHandler = (): void => {
+    setTitle("Updated!");
+    console.log(title);
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2> {props.title}</h2>
+        <h2> {title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 };
