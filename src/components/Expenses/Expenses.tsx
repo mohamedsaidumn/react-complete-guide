00c6a1c03aeae017f1192212ef41194ff9f9a56e
mohamedsaid321 <mohamedsaidumn@gmail.com>
2023-02-28 23:31:42 -0600

    added a controlled component called ExpensesFilter which passes the selected year to the parent Expenses

--- src/components/Expenses/Expenses.tsx
+++ src/components/Expenses/Expenses.tsx
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import { ExpenseItemWithIdType } from "../../config/types";
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
 
 const Expenses = (props: { expenses: ExpenseItemWithIdType[] }) => {
   const { expenses } = props;
+  const [filteredYear, setFilteredYear] = useState("2020");
+
   const listExpenses = expenses.map((expense) => {
     return (
       <ExpenseItem
@@ -16,7 +19,20 @@ const Expenses = (props: { expenses: ExpenseItemWithIdType[] }) => {
     );
   });
 
-  return <Card className="expenses">{listExpenses}</Card>;
+  const filterChangeHandler = (selectedYear: string) => {
+    setFilteredYear(selectedYear);
+    console.log(selectedYear);
+  };
+
+  return (
+    <Card className="expenses">
+      <ExpensesFilter
+        selected={filteredYear}
+        onChangeFilter={filterChangeHandler}
+      />
+      {listExpenses}
+    </Card>
+  );
 };
 
 export default Expenses;
