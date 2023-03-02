345a4598848adaa22ebccfcaf6c7c5475b925a9a
mohamedsaid321 <mohamedsaidumn@gmail.com>
2023-03-01 20:24:07 -0600

    moved the logic of conditional rendering to its own component

--- src/components/Expenses/Expenses.tsx
+++ src/components/Expenses/Expenses.tsx
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 import { ExpenseItemWithIdType } from "../../config/types";
-import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
 
 const Expenses = (props: { expenses: ExpenseItemWithIdType[] }) => {
   const { expenses } = props;
@@ -13,28 +13,9 @@ const Expenses = (props: { expenses: ExpenseItemWithIdType[] }) => {
     setFilteredYear(selectedYear);
   };
 
-  const listExpenses = expenses.map((expense) => {
-    return (
-      <ExpenseItem
-        key={expense.id}
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-      />
-    );
+  const filteredExpensesByYear = expenses.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
   });
-
-  const filteredExpensesByYear = listExpenses.filter((expense) => {
-    return expense.props.date.getFullYear().toString() === filteredYear;
-  });
-
-  const expensesContent = () => {
-    if (filteredExpensesByYear.length === 0) {
-      return <p>No Expenses Found</p>;
-    } else {
-      return filteredExpensesByYear;
-    }
-  };
 
   return (
     <Card className="expenses">
@@ -42,7 +23,7 @@ const Expenses = (props: { expenses: ExpenseItemWithIdType[] }) => {
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
-      {expensesContent()}
+      <ExpensesList filteredExpenses={filteredExpensesByYear} />
     </Card>
   );
 };
